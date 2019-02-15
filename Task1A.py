a7a5c0b8cd7378eb6dc531308e8faf6312290200
deepakkumar.seku@dal.ca <deepakkumar.seku@dal.ca>
2019-02-15 10:35:38 -0800

    I have pinted the words in descending order with respect to their values. To do this I have taken code from previous task, modified it in a way that data from dictionary d is taken and is appended into a list, that list now contains keys and values of each word in an order. Then I used Lambda function to sort to list in decsending order.

--- Task1A.py
+++ Task1A.py
@@ -53,6 +53,8 @@ def lower(book1,book2,book3):
 
 	count_the_article(res1,res2,res3)
 
+	sorted_list(res1,res2,res3,d1,d2,d3)
+
 def unique_words(d1,d2,d3):
 	for keys in d1:
 		print(keys)
@@ -74,6 +76,45 @@ def count_the_article(res1,res2,res3):
 		c3 = c3+1
 	print("The total number of words in Book3 = ",c3)
 
+def sorted_list(res1,res2,res3,d1,d2,d3):
 
+	lt1 = []
+	sub1 = []
+	t1 = d1.items()
+	for key,value in t1:
+		lt1.append(key)
+		lt1.append(value)	
+
+	for i in range(0,len(lt1),2):
+		sub1.append(lt1[i:i+2])
+	sub1.sort(key = lambda s:s[1],reverse = True)
+
+	print(sub1)
+
+	lt2 = []
+	sub2 = []
+	t2 = d2.items()
+	for key,value in t2:
+		lt2.append(key)
+		lt2.append(value)        
+
+	for i in range(0,len(lt2),2):
+		sub2.append(lt2[i:i+2])
+	sub2.sort(key = lambda s:s[1],reverse = True)
+
+	print(sub2)
+
+	lt3 = []
+	sub3 = []
+	t3 = d3.items()
+	for key,value in t3:
+		lt3.append(key)
+		lt3.append(value)        
+
+	for i in range(0,len(lt3),2):
+		sub3.append(lt3[i:i+2])
+	sub3.sort(key = lambda s:s[1],reverse = True)
+
+	print(sub3)
 
 lower('Book1.txt','Book2.txt','Book3.txt')
